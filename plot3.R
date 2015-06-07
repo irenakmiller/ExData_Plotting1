89c791874503606b31d7f0c9e5a0a771544d884c
irenakmiller <irenakmiller@gmail.com>
2015-06-07 15:33:02 -0400

    Plot 3 - Coursera - Exploratory Data

--- plot3.R
+++ plot3.R
@@ -2,7 +2,7 @@
 # Coursera - Exploratory Data Analysis
 
 # Get the full dataset
-energyFile <- file("household_power_consumption.txt")
+energyFile <- file("~/Desktop/Coursera Data Science Track/Exploratory Data Analysis/Project 1/household_power_consumption.txt")
 
 fullData <- read.csv(energyFile, header = T, sep = ';', 
                      na.strings = "?", nrows = 2075259, check.names = F, 
@@ -22,11 +22,11 @@ data$Datetime <- as.POSIXct(datetime)
 # Generate Plot 3
 with(data, {
         plot(Sub_metering_1 ~ Datetime, type = "l", 
-             ylab = "Global Active Power (kilowatts)", xlab = "")
+             ylab = "Energy sub metering", xlab = "")
         lines(Sub_metering_2 ~ Datetime, col = 'Red')
         lines(Sub_metering_3 ~ Datetime, col = 'Blue')
 })
-legend("topright", col = c("black", "red", "blue"), lty = 1, lwd = 2, 
+legend("topright",  cex=0.50, col = c("black", "red", "blue"), lty = 1, lwd = 2, 
        legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
 
 # Save plot to a file
